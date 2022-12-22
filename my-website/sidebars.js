7d4670b3e56465c26e52efdae94bbc449ad6f637
ljunggren <mljunggren@gmail.com>
2022-12-21 23:28:22 -0500

    Updated UI

---
+++ my-website/sidebars.js
@@ -0,0 +1,49 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Introduction',
+      collapsed: false,
+      items: [
+        "introduction/onboarding",
+        "introduction/glance",
+        "introduction/why",
+        "introduction/when"
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Getting Started',
+      collapsed: false,
+      items: [
+        "getting-started/signing-up",
+        "getting-started/creating",
+        "getting-started/modules-and-tests",
+        "getting-started/recording",
+        "getting-started/in-tool-help",
+      ],
+    },
+    {
+      type: 'category',
+      label: 'An End-to-End example',
+      collapsed: false,
+      items: [
+        'cucumber-google/introduction',
+        'cucumber-google/feature',
+        'cucumber-google/importing',
+        'cucumber-google/implement',
+        'cucumber-google/linking',
+        'cucumber-google/dry-run', 
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Element Selectors',
+      collapsed: false,
+      items: [
+        'element-selectors/welcome',
+
+      ],
+    }
+  ],
+};
