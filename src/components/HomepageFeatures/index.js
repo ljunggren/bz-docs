ea75f4e7435a2cfcb26018f5382142a1d0e41104
ljunggren <mljunggren@gmail.com>
2022-12-24 18:03:25 -0500

    Update front page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,28 +8,25 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Boozang was designed from the ground up to allow you to easily get end-to-end test coverage.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Cucumber Support',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Cucumber support is built-in, and we integrate to both Xray and all VCS platforms: GitHub, BitBucket, GitLab, Azure. 
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Natural-language selectors',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Our natural-language selectors are very stable to code-changes, and allow for model-based testing.
       </>
     ),
   },
